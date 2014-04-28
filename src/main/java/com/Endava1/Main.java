4e6021b66f1a2ec38319c46527d411b6a10bc898
catalincatana <catalin.catana3@gmail.com>
2014-04-28 17:12:43 +0300

    maa

---
+++ src/main/java/com/Endava1/Main.java
@@ -0,0 +1,12 @@
+package com.endava;
+
+/**
+ * Created by ccatana on 4/28/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Primul Program");
+        System.out.println("Al doilea program");
+		System.out.printl("ss");
+    }
+}
